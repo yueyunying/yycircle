d80433930e00cc353a9a7c5015e2dbd524afa897
乐运应 <978840018@qq.com>
2025-07-24 15:39:32 +0800

    增加字段

--- join-job/src/util/FillDataTransformer.php
+++ join-job/src/util/FillDataTransformer.php
@@ -21,11 +21,16 @@ class FillDataTransformer
             'member_name'            => $this->employee->member_name,
             'adjusted_checkin_time'  => $this->adhocJob->job_start_date,
             'adjusted_checkout_time' => $this->adhocJob->job_end_date,
+            'break_time_from'        => $this->adhocJob->break_time_from ? Carbon::parse($this->adhocJob->break_time_from)->timestamp: 0,
+            'break_time_to'          => $this->adhocJob->break_time_to ? Carbon::parse($this->adhocJob->break_time_to)->timestamp: 0,
             'is_send'                => 1,
             'add_time'               => now()->timestamp,
             'source'                 => $this->source,
             'adjusted_hourly_rate'   => $this->adhocJob->job_hour_rate,
             'work_status'            => $this->adhocJob->employer_status != 1 ? 13 : 2,
+            'cancel_status'          => 0,
+            'cancel_reason'          => '',
+            'agency_rate'            => $this->adhocJob->agency_rate,
         ];
         $data['job'] = [];
         if ($this->adhocJob->employer_status != 1){
